3b63d65f7cd1c9f0963956f031b9fb3342593aa0
astrhuetrh <136999162+astrhuetrh@users.noreply.github.com>
2023-06-22 23:58:08 +0300

    Update uygulama1.cpp

---
+++ uygulama1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void byteYazdirma(const std::string& dosyaYolu) {
+    std::ifstream file(dosyaYolu, std::ios::binary);
+    if (!file.is_open()) {
+        std::cout << "Dosya acilamadi: " << dosyaYolu << std::endl;
+        return;
+    }
+
+    char bytes[10];
+    file.read(bytes, 10);
+    file.close();
+
+    std::cout << "Dosya adi: " << dosyaYolu;
+    std::cout << "\nIlk 2 byte: ";
+        for(int n=0;n<2;n++){
+            std::cout << bytes[n];
+    };
+}
+
+int main() {
+    std::string dosya;
+    std::cout << "Dosya yolu giriniz:";
+    std::getline(std::cin,dosya);
+    byteYazdirma(dosya);
+    return 0;
+}
